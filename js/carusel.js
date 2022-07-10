1f29f0f2257bfe0543ab847927075d82e21b0e5a
Alex Makarov <rubenen@mail.ru>
2022-07-10 23:37:37 +0300

    commit

--- js/carusel.js
+++ js/carusel.js
@@ -1,25 +1,35 @@
-const sliderList = document.querySelectorAll('.slider__item');
-const sliderBtn = document.querySelector('.slider__btn');
+const btnNext = document.querySelector(".slider__btn-next");
+const btnPrev = document.querySelector(".slider__btn-prev");
+
+const size = document.querySelector(".slider").clientWidth;
+const amountItems = document.querySelectorAll(".slider__item").length;
+const shiftSize = size * amountItems;
 let count = 0;
 
+console.log(size);
 
 
-document.addEventListener('click',(elem) => {
-    if (elem.target.className === 'slider__btn slider__btn-next') {
-        sliderList[count + 1].classList.add('slider__item-active');
-        setTimeout(()=> {
-            sliderList[count - 1].classList.remove('slider__item-active');            
-        }, 100);
+const sliderContainer = document.querySelector(".slider__container");
+
+btnNext.addEventListener("click", ()=> {
+    if (count < amountItems - 1) {
         count++;
-    }
-    if (elem.target.className === 'slider__btn slider__btn-prev') {
-        sliderList[count - 1].classList.add('slider__item-active');
-        setTimeout(() => {
-            sliderList[count + 1].classList.remove('slider__item-active');
-        },100);
-        count--;
-    }
+        sliderContainer.style.transform = `translateX(${-size * count}px)`;
+        console.log(count)
+    }  
 });
+
+
+btnPrev.addEventListener("click", () => {
+    if (count > 0) {
+        count--;
+        sliderContainer.style.transform = `translateX(${-size * count}px)`;
+    }  
+});
+
+
+
+
 
 
 
